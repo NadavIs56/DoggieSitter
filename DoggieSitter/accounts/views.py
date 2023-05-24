71ba624e3b0f2cdf4e28430e33c850e03b1f0a3d
Nadav Ishai <73107945+NadavIs56@users.noreply.github.com>
2023-05-24 15:02:38 +0300

    Update views.py

--- DoggieSitter/accounts/views.py
+++ DoggieSitter/accounts/views.py
@@ -178,7 +178,7 @@ def Add(request):
 def Vet_Map(request, un):
     check = User.objects.get(username=un)
     check2 = Accounts.objects.get(user=check)
-    API_KEY = "AIzaSyA1NSKaMXW4cC5k9RB8dtOqlfZq9v7FNHc"
+    API_KEY = "GOOGLE_API_KEY"
     map_client = googlemaps.Client(API_KEY)
     app = Nominatim(user_agent="tutorial")
 
@@ -245,7 +245,7 @@ class DogPage(View):
 
 
 def Parks(request, un):
-    API_KEY = "AIzaSyA1NSKaMXW4cC5k9RB8dtOqlfZq9v7FNHc"
+    API_KEY = "GOOGLE_API_KEY"
     map_client = googlemaps.Client(API_KEY)
     app = Nominatim(user_agent="tutorial")
 
